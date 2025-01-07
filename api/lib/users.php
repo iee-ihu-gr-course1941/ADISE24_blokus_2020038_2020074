e62d5c4b5330814a89325ec76b2703d72c05d4d6
NoctKK <106299991+NoctKK@users.noreply.github.com>
2025-01-07 21:14:59 +0000

    Add files via upload

--- api/lib/users.php
+++ api/lib/users.php
@@ -3,6 +3,27 @@ require_once "dbcon.php";
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+function authUser($username,$password): void{
+	global $conn;
+	$sql = 'select * from users where username=? and password=?';
+	$st = $conn->prepare($sql);
+	$st->bind_param('ss', $username, $password);
+	$st->execute();
+	$res = $st->get_result();
+	$r = $res->fetch_assoc();
+
+	if ($r['session_id'] == '') {
+		http_response_code(401);
+		echo json_encode("Wrong credentials");
+		echo json_encode($username . ' ' . $password);
+	}else{
+		$_SESSION['TOKEN'] = $r["session_id"];
+		http_response_code(200);
+		echo json_encode('Successful Login');
+	}
+
+}
+
 function createUser(string $username,string $password): void{
 	global $conn;
 	$sql = 'select createUser(?,?)';
@@ -19,3 +40,25 @@ function createUser(string $username,string $password): void{
 		echo json_encode("Could not create user");
 	}
 }
+
+function showUserInfo(): void{
+	global $conn;
+	$sql = 'select getUserInfo(?)';
+	$st = $conn->prepare($sql);
+	$st->bind_param('s',$_SESSION['TOKEN']);
+	$st->execute();
+	$res= $st->get_result();
+	$r = $res->fetch_assoc();
+	print_r($r['getUserInfo(?)']);
+}
+
+function getScoreboard(): void{
+	global $conn;
+	$sql = 'select getScoreboard()';
+	$st = $conn->prepare($sql);
+	$st->execute();
+	$res= $st->get_result();
+	$r = $res->fetch_assoc();
+	print_r($r['getScoreboard()']);
+}
+
